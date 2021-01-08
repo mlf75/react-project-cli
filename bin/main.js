054905c43db137c4676a249ec3fb11dbf86fc638
mlf75 <443536193@qq.com>
2021-01-08 18:52:41 +0800

    save

---
+++ bin/main.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+const program = require('commander')
+
+program.version(require('../package.json').version)
+program.command('init <name>')
+  .description('初始化项目结构')
+  .action(
+    require('../lib/init')
+  )
+
+program.parse(process.argv)
